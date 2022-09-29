00a94283fd6313db7809ba8c99aff659da317dc4
lukhenthiran.t <lukhenthiran.t@192.168.56.1>
2022-09-29 11:52:45 +0800

    commit

---
+++ Test Suites/Web.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Web</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cadbd04c-bef3-410f-8795-4ac08854aa33</testSuiteGuid>
+</TestSuiteEntity>
